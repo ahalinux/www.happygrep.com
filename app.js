515bd27c5eef9684c16dd015d6ec363e627f3967
icat-PC\icat <575713113@qq.com>
2017-03-02 21:32:21 +0800

    170302commit

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.set('views','./views');
+app.set('views','./views/pages');
 app.set('view engine','jade');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'bower_components')));
 
 app.listen(port);
 console.log('Server started on port ' + port);
@@ -11,6 +16,32 @@ console.log('Server started on port ' + port);
 // index page
 app.get('/', function(req,res){
     res.render('index',{
-        title: 'Hello World'
+        title: '首页',
+        movies:[{
+            title:'机械战警',
+            _id:1,
+            poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+        }]
+    })
+})
+
+// detail page
+app.get('/article/:id', function(req,res){
+    res.render('detail',{
+        title: '详情页'
+    })
+})
+
+// admin page
+app.get('/admin/article', function(req,res){
+    res.render('admin',{
+        title: '后台录入页'
+    })
+})
+
+// list page
+app.get('/admin/list', function(req,res){
+    res.render('list',{
+        title: '列表页'
     })
 })
